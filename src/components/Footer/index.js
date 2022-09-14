0f705bae4cc030ec95035237766ded3baef47b99
llucassilva <llucassilvap@gmail.com>
2022-09-14 11:28:24 -0300

    home

---
+++ src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import "./footer.css";
+
+export default function Footer(){
+    return(
+        <footer>
+            {/* <img src="assets/321viagens.png" alt='logo da empresa'className='logo-footer'/> */}
+            <p className='logo'> 321 viagens</p>
+
+            <p className='texto-footer'>Copyright &copy; - 321viagens  </p>
+        </footer>
+    )
+}
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,78 @@
 import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Footer from '../../components/Footer';
+import Header from '../../components/Header';
+import './style.css';
+import Form from 'react-bootstrap/Form';
+
 
 
 function Home() {
   return (
     <div>
-        <h1>Home</h1>
+      <Header rota="/cadastro"/>
+
+
+      <div className='topo'>
+
+        <div  className='imagem-aviao'>
+          <img src='assets/imagem_aviao.png' alt='imagem de um aviao'/> 
+        </div>
+
+        <div className='container dados-passagens'>
+ 
+          <div className='conatiner align-items-center  d-flex p-2 mt-4'>
+            <Form.Select aria-label="Default select example" className='idaevolta m-0'>
+              <option>Ida e Volta</option>
+              <option value="1">Somente ida</option>  
+            </Form.Select>
+
+            <Form.Select aria-label="Default select example" className='economico m-0 mx-3'>
+              <option>econômica</option>
+              <option value="1">primeira classe</option>
+              <option value="2">executiva</option>
+            </Form.Select>
+
+            <Form.Select aria-label="Default select example" className='umpassageiro m-0'>
+              <option>1 passageiro</option>
+              <option value="1">2 passageiros</option>
+              <option value="2">3 ou +</option>
+            </Form.Select>
+          </div>
+          
+          <div className='mt-4 d-flex flex-direction-row align-itens-center p-2'>
+            
+            <Form.Control
+              className='origem w-40'
+              type="text"
+              placeholder="Digite a origem"
+            />
+            <Form.Control
+              className='destino w-40'
+              type="text"
+              placeholder="Digite o destino"
+            />
+            <Form.Control
+              className='ida w-40'
+              type="text"
+              placeholder="Ida : Data"
+            />
+            <Form.Control
+              className='volta w-40'
+              type="text"
+              placeholder="Volta : Data"
+            />
+
+          </div>
+
+
+          
+        </div>
+        
+      </div>
+
+
+        <Footer/>
     </div>
   )
  
