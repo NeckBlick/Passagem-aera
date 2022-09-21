f28f86174b2f9e37fda0c3aa13671c40f0b8eb63
NeckBlick <nickmestre200@gmail.com>
2022-09-21 17:29:41 -0300

    back-end home, faltando alguns detalhes

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
@@ -6,15 +6,55 @@ import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import "./home.css";
 
-
 function Home() {
+  const [voos, setVoos] = useState([]);
+  const [table, setTable] = useState();
+  useEffect(() => {
+    const buscarVoos = async (e) => {
+      await fetch("http://localhost/api/buscarvoos.php")
+        .then((response) => response.json())
+        .then((data) => {
+          setVoos(data);
+        });
+    };
+    buscarVoos();
+  }, []);
+  const exibirVoos = () => {
+    if(voos[0].error){
+      setTable(
+        <h1 className="display-4">Voo não encontrado</h1>
+      )
+    }else{
+
+    setTable(<table>
+      <tr>
+        <th className="fs-2 p-2 text-center bg-danger">Companhia</th>
+        <th className="fs-2 p-2 text-center bg-danger">Dia do Voo</th>
+        <th className="fs-2 p-2 text-center bg-danger">Destino</th>
+      </tr>
+      {voos.map((voo) => {
+        return (
+          <tr key={voo.id}>
+            <td className="fs-4 text-center p-2">{voo.companhia}</td>
+            <td className="fs-4 text-center p-2">{voo.diavoo}</td>
+            <td className="fs-4 text-center p-2">{voo.destino}</td>
+          </tr>
+        );
+      })}
+    </table>)
+    }
+  }
   return (
     <div>
       <Header value="Cadastrar" rota="/cadastro" />
 
       <div className="topo">
         <div className="imagem-aviao">
-          <img className="aviao_imagem" src="assets/imagem_aviao.png" alt="imagem de um aviao" />
+          <img
+            className="aviao_imagem"
+            src="assets/imagem_aviao.png"
+            alt="imagem de um aviao"
+          />
         </div>
 
         <div className="container dados-passagens ">
@@ -77,93 +117,107 @@ function Home() {
             </Form.Select>
 
             <div className="data_viagem">
-            <Form.Control
-              className="ida origem w-40"
-              type="date"
-              placeholder="Ida : Data"
-            />
-            <Form.Control
-              className="volta  origem  w-40"
-              type="date"
-              placeholder="Volta : Data"
-            />
-            <div className="botao_buscar ">
-
-            <Button variant="danger" className="botao_voos">
-              Buscar
-            </Button>{" "}
-            </div>
+              <Form.Control
+                className="ida origem w-40"
+                type="date"
+                placeholder="Ida : Data"
+              />
+              <Form.Control
+                className="volta  origem  w-40"
+                type="date"
+                placeholder="Volta : Data"
+              />
+              <div className="botao_buscar ">
+                <Button variant="danger" className="botao_voos" onClick={exibirVoos}>
+                  Buscar
+                </Button>{" "}
+              </div>
             </div>
           </div>
         </div>
       </div>
+      
+      <div className="busca">
+        <div className="container busca p-1">
+          {table}
+        </div>
+      </div>
 
-      <div className="inferior_baixo">
+      <div className="inferior_baixo ">
         <div className="w-100 d-flex justify-content-center align-items-center">
           <p className="texto_oferta">Ofertas a partir de R$380,57</p>
-        </div>  
-
-        <div className=" container mb-6">
-              <div className="row flex-wrap">
-                <div className="col-12 col-sm-6 col-md-4 col-lg-4 d-flex justify-content-center align-items-center my-3">
-                  <div className="card_home">
-                    <div className="img-top w-100">
-                      <img src="assets/imagem_cidade_1.png" alt="imagem de uma cidade" className="w-100 img-fluid"/>
-                    </div>
-                    <div className="bottom-card">
-                      <p className="title">Rio de Janeiro</p>
-                      <div className="bottom-ida">
-                        <p>Somente ida</p>
-                        <p>Economica</p>
-                      </div>
-                      <div className="price">
-                        <span>Viagens a partir de</span>
-                        <p>R$ 380,57</p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div className="col-12 col-sm-6 col-md-4 col-lg-4 d-flex justify-content-center align-items-center my-3">
-                  <div className="card_home">
-                    <div className="img-top w-100">
-                      <img src="assets/imagem_cidade_02.png" alt="imagem de uma cidade" className="w-100 img-fluid"/>
-                    </div>
-                    <div className="bottom-card">
-                      <p className="title">Fernando de Noronha</p>
-                      <div className="bottom-ida">
-                        <p>Ida e Volta</p>
-                        <p>Economica</p>
-                      </div>
-                      <div className="price">
-                        <span>Viagens a partir de</span>
-                        <p>R$ 473,41</p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div className="col-12 col-sm-6 col-md-4 col-lg-4 d-flex justify-content-center align-items-center my-3">
-                  <div className="card_home">
-                    <div className="img-top w-100">
-                      <img src="assets/imagem_cidade_03.png" alt="imagem de uma cidade" className="w-100 img-fluid"/>
-                    </div>
-                    <div className="bottom-card">
-                      <p className="title">Salvador</p>
-                      <div className="bottom-ida">
-                        <p>Somente ida</p>
-                        <p>Economica</p>
-                      </div>
-                      <div className="price">
-                        <span>Viagens a partir de</span>
-                        <p>R$ 423,10</p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                
-
-              </div>
         </div>
 
+        <div className=" container mb-6">
+          <div className="row flex-wrap">
+            <div className="col-12 col-sm-6 col-md-4 col-lg-4 d-flex justify-content-center align-items-center my-3">
+              <div className="card_home">
+                <div className="img-top w-100">
+                  <img
+                    src="assets/imagem_cidade_1.png"
+                    alt="imagem de uma cidade"
+                    className="w-100 img-fluid"
+                  />
+                </div>
+                <div className="bottom-card">
+                  <p className="title">Rio de Janeiro</p>
+                  <div className="bottom-ida">
+                    <p>Somente ida</p>
+                    <p>Economica</p>
+                  </div>
+                  <div className="price">
+                    <span>Viagens a partir de</span>
+                    <p>R$ 380,57</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-12 col-sm-6 col-md-4 col-lg-4 d-flex justify-content-center align-items-center my-3">
+              <div className="card_home">
+                <div className="img-top w-100">
+                  <img
+                    src="assets/imagem_cidade_02.png"
+                    alt="imagem de uma cidade"
+                    className="w-100 img-fluid"
+                  />
+                </div>
+                <div className="bottom-card">
+                  <p className="title">Fernando de Noronha</p>
+                  <div className="bottom-ida">
+                    <p>Ida e Volta</p>
+                    <p>Economica</p>
+                  </div>
+                  <div className="price">
+                    <span>Viagens a partir de</span>
+                    <p>R$ 473,41</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-12 col-sm-6 col-md-4 col-lg-4 d-flex justify-content-center align-items-center my-3">
+              <div className="card_home">
+                <div className="img-top w-100">
+                  <img
+                    src="assets/imagem_cidade_03.png"
+                    alt="imagem de uma cidade"
+                    className="w-100 img-fluid"
+                  />
+                </div>
+                <div className="bottom-card">
+                  <p className="title">Salvador</p>
+                  <div className="bottom-ida">
+                    <p>Somente ida</p>
+                    <p>Economica</p>
+                  </div>
+                  <div className="price">
+                    <span>Viagens a partir de</span>
+                    <p>R$ 423,10</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
 
       <Footer />
